a5886e433549565d904ecd08e0f3c7811f25e6a7
YogaDharma21 <yogacode2006@gmail.com>
2026-02-22 20:06:41 +0800

    feat(navbar): add responsive navigation component

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+let menuBtn = document.getElementById("menuBtn");
+let navLinks = document.getElementById("navLinks");
+
+menuBtn.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+
+    if (navLinks.classList.contains("active")) {
+        menuBtn.textContent = "[CLOSE]";
+    } else {
+        menuBtn.textContent = "[MENU]";
+    }
+});
